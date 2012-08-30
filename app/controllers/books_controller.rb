ec2cac60dbdb625dc25df3f267427f08303470f2
Alexey Zapriy <alexey.zapriy@gmail.com>
2012-08-30 11:43:50 +0300

    Added pagination using will_paginate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
-
+    @books = Book.paginate(:page => params[:page])
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @books }
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ActiveRecord::Base
   attr_accessible :content, :title
   
   belongs_to :user
+  
+  self.per_page = 2
 end
